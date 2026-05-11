55de377d14ffb9b1b6e459730b52caf42e1f6de1
chienpanglee <lee.chienpang@gmail.com>
2026-05-11 06:08:49 +0000

    Update from https://github.com/chienpanglee/chienpanglee.blog/commit/115471f4e8d5c74c1277903069cd5f141290becd

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1778479298',resources: [
+    cacheName: 'chirpy-1778479727',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
